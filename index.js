f8ac92bc230be876d56d7e7088280799bd9177c4
Andrew Bastin <andrewbastin.k@gmail.com>
2020-09-16 20:36:13 -0400

    Updated to provide highlight for hoppscotch env vars only

--- index.js
+++ index.js
@@ -10,105 +10,29 @@ const selectionOutput = document.getElementById('selection');
 const errors = document.getElementById("errors");
 
 function parseURL(text) {
-  console.log(`"${text}"`);
+  const timeBegin = Date.now();
 
-  // Timing
-  const timeStart = Date.now();
+  const map = [];
+  const regex = /<<\w+>>/;
 
-  // Results
-  let protocol = null;
-  let domain = null;
-  let path = null;
-  let queryPart = null;
-  let fragment = null;
+  let match;
+  let index = 0;
+  while (match = text.substring(index).match(regex)) {
+    map.push([index, index + (match.index - 1), 'TEXT']);
+    map.push([index + match.index, index + match.index + match[0].length - 1, 'VAR']);
+    index += match.index + match[0].length;
 
-  let scanPos = 0;
-  let map = [];
-
-  const eatTill = (str, tag) => {
-    const index = text.substring(scanPos).indexOf(str);
-    if (index == -1) throw new Error(`Expected to find an occurance of '${str}'`);
-
-    const result = text.substring(scanPos, scanPos + index);
-
-    map.push([scanPos, scanPos + index - 1, tag]);
-    scanPos += index;
-
-    return result;
+    if (index >= text.length - 1) break;
   }
 
-  const eat = (str, tag) => {
-    if (!text.substring(scanPos).startsWith(str)) throw new Error(`Cannot eat because expected to find '${str}'`);
-
-    map.push([scanPos, scanPos + str.length - 1, tag]);
-    scanPos += str.length;
+  if (text.length > index && !text.substring(index).match(regex)) {
+    map.push([index, text.length, 'TEXT']);
   }
 
-  const eatAll = (tag) => {
-    const value = text.substring(scanPos);
+  parseTime = Date.now() - timeBegin;
 
-    map.push([scanPos, text.length - 1, tag]);
-    scanPos = text.length;
-
-    return value;
-  }
-
-  const has = (str) => {
-    return text.substring(scanPos).includes(str);
-  }
-
-  // Actual parsing grammar
-  try {
-    protocol = eatTill("://", "PROTOCOL");
-    eat("://", "TEXT");
-
-    domain = eatTill("/", "DOMAIN");
-    eat("/", "TEXT");
-    
-    if (has("?")) {
-      path = eatTill("?", "PATH");
-
-      eat("?", "TEXT");
-
-      if (has("#")) {
-        queryPart = eatTill("#", "QUERY");
-
-        eat("#", "TEXT");
-
-        fragment = eatAll("FRAGMENT");
-      } else {
-        queryPart = eatAll("QUERY");
-      }
-    } else {
-      if (has("#")) {
-        path = eatTill("#", "PATH");
-
-        fragment = eatAll("FRAGMENT");
-      } else {
-        path = eatAll("PATH");
-      }
-    }
-    
-    eatAll("TEXT");
-
-    parseErrors = null;
-  } catch (e) {
-    parseErrors = e;
-    eatAll("TEXT");
-  }
-
-  // Timing
-  parseTime = Date.now() - timeStart;
-
-  map = map.filter(([start, end]) => start <= end);
-
-  return {
-    protocol,
-    domain,
-    queryPart,
-    fragment,
-    map
-  }
+  parsedData = map;
+  return { map };
 }
 
 
@@ -199,8 +123,13 @@ function renderText(text) {
 
   parsedData = parseData;
 
+  const convertSpan = document.createElement('span');
+  
   const output = parseData.map.map(([start, end, protocol]) => {
-    return `<span class='highlight highlight-${protocol}'>${fixedText.substring(start, end + 1)}</span>`
+
+    convertSpan.textContent = fixedText.substring(start, end + 1);
+    console.log(convertSpan.innerHTML);
+    return `<span class='highlight-${protocol}'>${convertSpan.innerHTML}</span>`;
   })
 
   return output.join('');
